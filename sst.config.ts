cced58a3cfe315d71a236faa45f9e69334e15e28
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2023-01-24 20:49:56 +0000

    initial commit from beta v2

---
+++ sst.config.ts
@@ -0,0 +1,23 @@
+import { SSTConfig } from "sst";
+import { API } from "./stacks/MyStack";
+
+export default {
+  config(input) {
+    const PROFILE: Record<string, string> = {
+      development: "development",
+      default: "development",
+    };
+    return {
+      name: "my-sst-app-rc",
+      region: "us-east-1",
+      profile: (input.stage && PROFILE[input.stage]) || PROFILE.default,
+    };
+  },
+  stacks(app) {
+    app.setDefaultFunctionProps({
+      runtime: "nodejs16.x",
+      architecture: "arm_64",
+    });
+    app.stack(API);
+  },
+} satisfies SSTConfig;
