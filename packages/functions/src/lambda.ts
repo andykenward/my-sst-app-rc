b2ccd1badc5ddad093802200bb71646fd3ae22a7
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2023-01-24 23:26:28 +0000

    refactor: lambda

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,9 +1,9 @@
 import { ApiHandler } from "sst/node/api";
+
 import { Time } from "@my-sst-app-rc/core";
 
 export const handler = ApiHandler(async (_evt) => {
-  console.log("bob");
   return {
-    body: `Hello world and not bob. The time is ${Time.now()}`,
+    body: `Hello world and some dogs. The time is ${Time.now()}`,
   };
 });
