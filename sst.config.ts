d0c7d9f4e66325cae77938ac00e1020b27b6337e
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2023-01-25 19:00:10 +0000

    feat: aws region for dev

--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,26 @@
 import { SSTConfig } from "sst";
 import { API } from "./stacks/MyStack";
 
+type Profile = "production" | "development" | "default";
+type Region = "us-east-1" | "eu-west-2";
+
 export default {
   config(input) {
     const PROFILE: Record<string, string> = {
       production: "production",
       development: "development",
       default: "development",
-    };
+    } satisfies Record<Profile, string>;
+
+    const REGION: Record<string, string> = {
+      production: "us-east-1",
+      development: "eu-west-2",
+      default: "eu-west-2",
+    } satisfies Record<keyof typeof PROFILE, Region>;
+
     return {
       name: "my-sst-app-rc",
-      region: "us-east-1",
+      region: (input.stage && REGION[input.stage]) || REGION.default,
       profile: (input.stage && PROFILE[input.stage]) || PROFILE.default,
     };
   },
